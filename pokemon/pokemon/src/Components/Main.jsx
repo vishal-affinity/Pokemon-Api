5570c5d8aa5f48b5b18aebcd038e3deda2fcd853
Vishal Kumar Singh <vishalkumar_singh@srmap.edu.in>
2022-09-26 19:42:09 +0530

    pokemon

---
+++ pokemon/pokemon/src/Components/Main.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+const Main=()=>{
+    const [pokeData,setPokeData]=useState([]);
+    const [loading,setLoading]=useState(true);
+    const [url,setUrl]=useState("https://pokeapi.co/api/v2/pokemon/")
+    const [nextUrl,setNextUrl]=useState();
+    const [prevUrl,setPrevUrl]=useState();
+    const [pokeDex,setPokeDex]=useState();
+
+    
+    const getPokemon=async(res)=>{
+       res.map(async(item)=>{
+          const result=await axios.get(item.url)
+          //console.log(result.data)
+          setPokeData(state=>{
+                state=[...state,result.data]
+                state.short((a,b)=>a.id>b.id?1:-1)
+                return state;
+          })
+       })
+    }
+    useEffect(()=>{
+        const pokeFun=async()=>{
+            setLoading(true)
+            const res=await axios.get(url);
+            //console.log(res.data.results)
+            setNextUrl(res.data.next);
+            setPrevUrl(res.data.Previous);
+            getPokemon(res.data.results)
+            setLoading(false)
+            //console.log(pokeData)
+        }
+        pokeFun();
+
+    },[url])
+    return(
+        <>
+            <div className="container">
+                <div className="left-content">
+                    <Card pokemon={pokeData} loading={loading} infoPokemon={poke=>setPokeDex(poke)}/>
+
+                    <div className="btn-group">
+                       {  prevUrl && <button onClick={()=>{
+                            setPokeData([])
+                            setUrl(prevUrl)
+                        }}>previous</button>}
+
+                       {nextUrl && <button onClick={()=>{
+                            setPokeData([])
+                            setUrl(nextUrl)
+                        }}>Next</button>}
+
+                    </div>   
+                </div>
+                <div className="right-content">
+                    <Pokeinfo data={pokeDex}/>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Main;
